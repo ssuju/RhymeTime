192eaa8cb729488513b04038630500465d052eb8
Jarred-528 <jarredanderson@unomaha.edu>
2019-11-15 13:17:46 -0600

    Changed edit profile

--- nurseryrhymes/rhymesapp/forms.py
+++ nurseryrhymes/rhymesapp/forms.py
@@ -22,7 +22,7 @@ class EditProfileForm(UserChangeForm):
 
     class Meta:
         model = User
-        fields = {'email', 'first_name', 'last_name'}
+        fields = {'last_name', 'email', 'first_name'}
 
 
 
