03b6d816344cdf2f592c0e8ed6f24f8e7fe2e215
Suju Sapkota <ssapkota@unomaha.edu>
2019-11-15 14:29:38 -0600

    account created.

--- nurseryrhymes/rhymesapp/forms.py
+++ nurseryrhymes/rhymesapp/forms.py
@@ -22,7 +22,7 @@ class EditProfileForm(UserChangeForm):
 
     class Meta:
         model = User
-        fields = {'first_name', 'email', 'last_name'}
+        fields = {'first_name', 'last_name', 'email'}
 
 
 
