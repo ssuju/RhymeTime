b6702a6acbc114f972ea3f84fe0aaf0bcc2a5ea2
emilybuettner <emilybuettner@unomaha.edu>
2019-11-19 21:55:58 -0600

    Tests

---
+++ nurseryrhymes/rhymesapp/tests/Nursery_Rhyme/Nursery_Test.py
@@ -0,0 +1,59 @@
+import unittest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+class Upgrade(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+
+    def test_upgrade(self):
+        user = "rhymetime"
+        pwd = "team3!"
+        driver = self.driver
+        driver.maximize_window()
+        driver.get("http://127.0.0.1:8000/login/")
+        elem = driver.find_element_by_id("id_username")
+        elem.send_keys(user)
+        elem = driver.find_element_by_id("id_password")
+        elem.send_keys(pwd)
+        elem.send_keys(Keys.RETURN)
+        driver.get("http://127.0.0.1:8000")
+        assert "Logged In"
+        time.sleep(5)
+
+        subject = "rhymetime"
+        Email = "ebuettner5@gmail.com"
+        message = "This is a selenium test"
+        elem = driver.find_element_by_xpath("/html/body/nav/div/div[2]/ul[1]/li[2]/a").click()
+        elem = driver.find_element_by_xpath("/html/body/div/div/div/div[1]/a[1]/h3").click()
+        elem = driver.find_element_by_xpath("/html/body/div/div/div/div[1]/a/span").click()
+
+
+
+        # elem = driver.find_element_by_id("id_subject")
+        # elem.send_keys(subject)
+        #
+        # time.sleep(5)
+        # elem = driver.find_element_by_id("id_email_address")
+        # elem.send_keys(Email)
+        # time.sleep(5)
+        #
+        # elem = driver.find_element_by_id("id_message")
+        # elem.send_keys(message)
+        # time.sleep(5)
+        #
+        # elem = driver.find_element_by_xpath("/html/body/nav/div/div[2]/ul[1]/li[2]/a").click()
+
+
+
+        time.sleep(10)
+
+    def tearDown(self):
+        self.driver.close()
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ nurseryrhymes/rhymesapp/tests/account_test/accout_info.py
@@ -0,0 +1,46 @@
+import unittest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+class Account_ATS(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+
+    def test_account(self):
+        user = "rhymetime"
+        pwd = "team3!"
+        driver = self.driver
+        driver.maximize_window()
+        driver.get("http://127.0.0.1:8000/login/")
+        elem = driver.find_element_by_id("id_username")
+        elem.send_keys(user)
+        elem = driver.find_element_by_id("id_password")
+        elem.send_keys(pwd)
+        elem.send_keys(Keys.RETURN)
+        driver.get("http://127.0.0.1:8000")
+        assert "Logged In"
+        time.sleep(5)
+        elem = driver.find_element_by_xpath("/html/body/nav/div/div[2]/ul[2]/li/a").click()
+        elem = driver.find_element_by_xpath("/html/body/nav/div/div[2]/ul[2]/li/ul/li[1]/a").click()
+        elem = driver.find_element_by_xpath("/html/body/div/div/div/form[1]/button").click()
+        time.sleep(5)
+
+        driver.get("http://127.0.0.1:8000/account_information/edit/")
+        elem2 = driver.find_element_by_id("id_first_name")
+        elem2.clear()
+        elem2.send_keys("test")
+        time.sleep(5)
+        elem = driver.find_element_by_xpath("/html/body/div/div/div/form/div/input").click()
+
+
+        time.sleep(5)
+
+    def tearDown(self):
+        self.driver.close()
+
+
+if __name__ == "__main__":
+    unittest.main()
