b6e05327cb48806d8bbd98865dfdc8adc7d4574b
Suju Sapkota <ssapkota@unomaha.edu>
2019-11-20 16:54:01 -0600

    trying

--- nurseryrhymes/nurseryrhymes/settings.py
+++ nurseryrhymes/nurseryrhymes/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
 ]
 
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.gmail.com:587'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'teamthreenurseryrhymes@gmail.com'
 EMAIL_HOST_PASSWORD = 'NurseryRhymes1!'
