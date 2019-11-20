59125fc250f0b3d39f632524f14dc4bf53eeaf8b
Suju Sapkota <ssapkota@unomaha.edu>
2019-11-19 23:27:14 -0600

    signup_test modified.

--- nurseryrhymes/rhymesapp/tests/signup/signup_test.py
+++ nurseryrhymes/rhymesapp/tests/signup/signup_test.py
@@ -1,14 +1,12 @@
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
+
 import unittest
 
 
-class UntitledTestCase(unittest.TestCase):
+class Register(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome()
         self.driver.implicitly_wait(30)
-        self.base_url = "https://www.katalon.com/"
         self.verificationErrors = []
         self.accept_next_alert = True
 
@@ -33,36 +31,6 @@ class UntitledTestCase(unittest.TestCase):
         driver.find_element_by_id("id_password_repeat").send_keys("gary")
         driver.find_element_by_xpath(
             "(.//*[normalize-space(text()) and normalize-space(.)='Confirm password:'])[1]/following::input[2]").click()
-
-    def is_element_present(self, how, what):
-        try:
-            self.driver.find_element(by=how, value=what)
-        except NoSuchElementException as e:
-            return False
-        return True
-
-    def is_alert_present(self):
-        try:
-            self.driver.switch_to_alert()
-        except NoAlertPresentException as e:
-            return False
-        return True
-
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.driver.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally:
-            self.accept_next_alert = True
-
-    def tearDown(self):
-        self.driver.quit()
-        self.assertEqual([], self.verificationErrors)
 
 
 if __name__ == "__main__":
