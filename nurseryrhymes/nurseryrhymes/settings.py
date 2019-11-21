f4df7bd2e05197a8c7783a58d48dd9f30e30e51e
Suju Sapkota <ssapkota@unomaha.edu>
2019-11-20 20:34:04 -0600

    changed host pass in settings.

--- nurseryrhymes/nurseryrhymes/settings.py
+++ nurseryrhymes/nurseryrhymes/settings.py
@@ -48,7 +48,9 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'teamthreenurseryrhymes@gmail.com'
-EMAIL_HOST_PASSWORD = 'NurseryRhymes1!'
+EMAIL_HOST_PASSWORD = 'xyhztcvscobvekpq'
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
