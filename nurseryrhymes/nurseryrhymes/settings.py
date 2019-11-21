e50a6cd1939e8db44539c14dde39e0ce88da764d
Suju Sapkota <ssapkota@unomaha.edu>
2019-11-20 20:43:50 -0600

    works.

--- nurseryrhymes/nurseryrhymes/settings.py
+++ nurseryrhymes/nurseryrhymes/settings.py
@@ -48,7 +48,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'teamthreenurseryrhymes@gmail.com'
-EMAIL_HOST_PASSWORD = 'xyhztcvscobvekpq'
+EMAIL_HOST_PASSWORD = 'hnjrmywkvrrtbpnp'
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
